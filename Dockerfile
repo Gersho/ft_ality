243127905cdc9445b4d638f0f9ca5de95767e659
Gersho <zennoune.karim@outlook.fr>
2023-09-25 22:49:10 +0200

    testing sdl

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ocaml/opam:debian-12-ocaml-4.14
 
 RUN sudo apt install -y pkg-config autoconf libsdl1.2-dev
 
-RUN eval $(opam env) && opam install -y ocamlsdl
+RUN eval $(opam env) && opam install -y dune ocamlsdl
 
 RUN sudo chown opam *
 
--- srcs/bin/main.ml
+++ srcs/bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+
+
+
+
+let main () =
+  Sdl.init [`VIDEO];
+  Format.printf "powered by libcaca";
+  let test =  Sdlevent.make_mask [KEYDOWN_EVENT; KEYUP_EVENT] in
+  Sdlevent.enable_events test;
+  Sdl.quit ()
+
+let _ = main () 
+
+
+(* let () = print_endline "Hello, World!" *)
