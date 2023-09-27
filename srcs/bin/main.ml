22287036e335cafb8da7f5c3ab0a4a5bc26fc28e
Gersho <zennoune.karim@outlook.fr>
2023-09-28 01:35:08 +0200

    more stuff

--- srcs/bin/main.ml
+++ srcs/bin/main.ml
@@ -1,42 +1,123 @@
 (* allowed modules
 Pervasives Sys List String Sdl Sdlevent Sdlkey *)
 
+type parsing_mode = Head | Keyconfig | Movelist
+
 type transition =
 {
-    read : string;
-    to_state : int;
-    write : string;
+  read : string;
+  to_state : int;
+  write : string;
 }
 
-type states = (int * transition list) list ;
+type state = 
+{
+  id : int;
+  transitions : transition list;
+}
 
-let () = 
-  (* Array forbiden module ?! *)
+type key =
+{
+  input_string : string;
+  output_string : string;
+(* temporary 
+  have to see how events work and how we get keysym/keycode info
+  input_keysym : Sdlkey.t; <- have to see how events work and *)
+}
+
+type full_config =
+{
+  keyconfig : key list;
+  machine : state list;
+}
+
+let  main () = 
+    (* Array forbiden module ?! *)
   (* TODO complete arg handling (help, debug, syntax) *)
   if Array.length Sys.argv < 2 then begin
-    failwith "Wrong amount of arguments";
+    print_endline "Wrong amount of arguments";
+    exit 1;
   end;
-
-  Format.printf "argv : %s\n" Sys.argv.(1);
+  print_string "Opening file: ";
+  print_endline Sys.argv.(1);
   try
     let gmr_file = open_in Sys.argv.(1) in
-    let rec parser accum = 
-      match input_line gmr_file with
+    let rec parser (accum :full_config) mode = 
+
+      match String.trim (input_line (gmr_file)) with
+      | line when String.equal line "@keyconfig" -> begin
+        print_endline "found keyconfig";
+        parser accum Keyconfig;
+      end;
+      | line when String.equal line "@movelist" -> begin
+        print_endline "found movelist";
+        parser accum Movelist;
+      end;
+
+      (* keyconfig parsing *)
+      | line when mode == Keyconfig -> begin
+        (* print_endline "in keyconf mode"; *)
+
+        (* -check is : is present
+        -split on :
+        -check for 2 non empty elem
+        -check accum if input not already present
+        -> add to accum or exit syntax error *)
+        
+        (* parser { keyconfig = [] ; machine = accum.machine ;} Head  *)
+        parser accum Keyconfig;
+      end;(* keyconfig parsing *)
+
+
+      (* movelist parsing *)
+      | line when mode == Movelist -> begin
+        (* print_endline "in movelist mode"; *)
+
+        -check is : is present
+        -split on :
+        -check for 2 non empty elem
+            first elem:
+            - check if - is present
+                -> if not, it is 1 action move
+            -split on -
+
+        ...check if 
+
+
+        parser accum Movelist;
+      end;(* movelist parsing *)
+
+
+
+
+
       | line -> begin
-        Format.printf "reading a line\n";
-        parser (List.append accum [line]);
+        (* print_endline "in head mode"; *)
+        parser accum Head; 
       end;
       | exception End_of_file -> (
-        Format.printf "finished reading the file\n";
+        print_endline "finished reading the file";
         close_in gmr_file;
         accum)
+
+
+
     in
-    let result = parser [] 
+    (* let result = parser ([], (_ , [])) Head  *)
+    let result = parser { keyconfig = [] ; machine = [];} Head 
     in
-    Format.printf "line count: %d\n" (List.length (result));
-    (* flush stdout; *)
+
+
+    (* debug print *)
+    print_string "line count: ";
+    (* print_int (List.length (result)); *)
+    print_endline "";
+    (* let print_list (elem) = print_endline elem in
+    List.iter print_list result; *)
+
+
   with e ->
-    raise e;
+    raise e
 
 
-
+let () = main ()
--- tmp/sdl_tests.ml
+++ tmp/sdl_tests.ml
@@ -1,11 +1,11 @@
-(* 
+
 let pwet () =  
     Sdlevent.wait_event ();
   Format.printf "allo\n"
 
- *)
 
-  (* let rec init () =
+
+  let rec init () =
     Format.printf "wesh\n";
     match Sdlevent.wait_event () with
     |  KEYDOWN {keysym=k} -> 
@@ -18,18 +18,18 @@ let pwet () =
       | KEY_q -> Sdl.quit ()
       | _ -> init ()
       )
-    | _ -> init () *)
+    | _ -> init ()
 
 
-  (* if Sdlevent.has_event () then begin
+  if Sdlevent.has_event () then begin
     match Sdlevent.wait_event () with
     | KEYDOWN _ -> Format.printf "DOWN";
     | KEYUP _ -> Format.printf "UP";
     | _ -> Format.printf "???";
   end;
-  Format.printf "allo\n" *)
+  Format.printf "allo\n"
 
-(* 
+
 let  handle_events () =
   let open Sdlevent in
   let open Sdlkey in
@@ -44,10 +44,10 @@ let  handle_events () =
   | Some ( KEYDOWN { keysym = KEY_LEFT } ) -> Format.printf "allo++\n"
   | Some ( KEYDOWN { keysym = KEY_RIGHT } ) -> Format.printf "allo++\n"
   | _ -> Sdltimer.delay 1000
- *)
 
 
- (* let sdl_init () =
+
+ let sdl_init () =
   Sdl.init [`EVERYTHING];
   Sdlevent.enable_events Sdlevent.all_events_mask
 
@@ -58,18 +58,16 @@ let rec wait_key () =
 	    | Sdlevent.KEYDOWN _ -> Format.printf "allo++\n"
       | _ -> wait_key ()
 
-let main () = sdl_init() ; wait_key() *)
+let main () = sdl_init() ; wait_key()
 
-(* let main () =
+let main () =
   Sdl.init [`VIDEO];
   Format.printf "powered by libcaca\n";
   let test =  Sdlevent.make_mask [KEYDOWN_EVENT; KEYUP_EVENT] in
   Sdlevent.enable_events test;
   Sdlevent.pump ();
   handle_events ();
-  handle_events ();
-  handle_events ();
-  Sdl.quit () *)
+  Sdl.quit ()
 
     (* init() *)
     (* pwet();
@@ -81,7 +79,7 @@ let main () = sdl_init() ; wait_key() *)
 (* let () = print_endline "Hello, World!" *)
 
 
-(* let handle_events () =
+let handle_events () =
   let open Sdlevent in
   let open Sdlkey in
   let open Sdlmouse in
@@ -96,7 +94,7 @@ let main () = sdl_init() ; wait_key() *)
   | Some ( KEYDOWN { keysym = KEY_DOWN } ) -> move_y (10.)
   | Some ( KEYDOWN { keysym = KEY_LEFT } ) -> move_x (-10.)
   | Some ( KEYDOWN { keysym = KEY_RIGHT } ) -> move_x (10.)
-  | _ -> () *)
+  | _ -> ()
 
 
 
@@ -118,3 +116,51 @@ let main () =
     wait_for_escape ()
 
 let _ = main ()
+
+
+
+
+(* https://stackoverflow.com/questions/7579135/problem-handling-keyboard-with-opengl-and-sdl-in-ocaml *)
+
+
+################################################################################
+################################################################################
+################################################################################
+current:
+
+
+(* let key_handler k = match k with
+| {Sdlevent.keysym=Sdlkey.KEY_ESCAPE} ->
+    print_endline "I'll miss you so much..." ;
+| {Sdlevent.keysym=Sdlkey.KEY_SPACE} ->
+  print_endline "UP" ;
+| {Sdlevent.keysym=Sdlkey.KEY_DOWN} ->
+  print_endline "DOWN";   
+| _ -> ()
+
+
+let rec wait_for_input () = 
+  Sdlevent.wait();
+  match Sdlevent.poll () with
+    | Some (Sdlevent.KEYDOWN k) ->
+  print_endline "event!"; key_handler k
+    | None | _ -> print_endline "nothing!(should not print)";
+  wait_for_input () *)
+
+
+  let rec wait_key () =
+    let e = Sdlevent.wait_event () in
+      match e with
+        | Sdlevent.KEYDOWN _ -> Format.printf "allo++\n"
+        | _ -> wait_key ()
+  
+  
+  let main () = 
+    Sdl.init [`VIDEO];
+    at_exit Sdl.quit;
+    let mask =  Sdlevent.make_mask [KEYDOWN_EVENT; KEYUP_EVENT] in
+    Sdlevent.enable_events mask;
+    (* wait_for_input () *)
+    wait_key ()
+  
+  let _ = main ()
