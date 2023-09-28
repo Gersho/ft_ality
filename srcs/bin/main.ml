3a4b8474217aa6a87a70314176c1793623ed65a1
Gersho <zennoune.karim@outlook.fr>
2023-09-28 12:39:11 +0200

    keyconfig parsing working (need more testing)

--- srcs/bin/main.ml
+++ srcs/bin/main.ml
@@ -42,38 +42,47 @@ let  main () =
   print_endline Sys.argv.(1);
   try
     let gmr_file = open_in Sys.argv.(1) in
-    let rec parser (accum :full_config) mode = 
-
+    let rec parser (accum :full_config) mode count = 
       match String.trim (input_line (gmr_file)) with
+      | "" -> parser accum mode (count + 1);
       | line when String.equal line "@keyconfig" -> begin
-        print_endline "found keyconfig";
-        parser accum Keyconfig;
-      end;
+        parser accum Keyconfig (count + 1); end;
       | line when String.equal line "@movelist" -> begin
-        print_endline "found movelist";
-        parser accum Movelist;
-      end;
-
-      (* keyconfig parsing *)
+        parser accum Movelist (count + 1); end;
       | line when mode == Keyconfig -> begin
-        (* print_endline "in keyconf mode"; *)
-
-        (* -check is : is present
+        (* keyconfig parsing
         -split on :
         -check for 2 non empty elem
         -check accum if input not already present
         -> add to accum or exit syntax error *)
-        
-        (* parser { keyconfig = [] ; machine = accum.machine ;} Head  *)
-        parser accum Keyconfig;
+        (* let key_as_list = String.split_on_char ':' line in *)
+        let duplicate_check  input elem = String.equal elem.input_string input in 
+        match String.split_on_char ':' line with
+        | input :: output when
+            (List.length output == 1) &&
+            (String.length input > 0) &&
+            (String.length (List.nth output 0) > 0) &&
+            not (List.exists (duplicate_check input) accum.keyconfig)
+            -> begin
+            let new_rec: key = { input_string = input; output_string = List.nth output 0;} in
+            parser { keyconfig = (List.rev_append accum.keyconfig [new_rec]) ; machine = accum.machine ;} Keyconfig (count + 1)
+          end;
+        | _ -> begin
+            print_string "Syntax error on line ";
+            print_int count;
+            print_endline "";
+            exit 3;
+          end;
       end;(* keyconfig parsing *)
+
+
 
 
       (* movelist parsing *)
       | line when mode == Movelist -> begin
         (* print_endline "in movelist mode"; *)
 
-        -check is : is present
+        (* 
         -split on :
         -check for 2 non empty elem
             first elem:
@@ -81,40 +90,65 @@ let  main () =
                 -> if not, it is 1 action move
             -split on -
 
-        ...check if 
+        ...check if  *)
 
 
-        parser accum Movelist;
+        (* let move_as_list = String.split_on_char ':' line in
+        match move_as_list *)
+
+        parser accum Movelist (count + 1);
       end;(* movelist parsing *)
 
 
 
 
 
-      | line -> begin
-        (* print_endline "in head mode"; *)
-        parser accum Head; 
+      | _ -> begin
+        parser accum Head (count + 1); 
       end;
       | exception End_of_file -> (
         print_endline "finished reading the file";
         close_in gmr_file;
         accum)
-
-
-
     in
-    (* let result = parser ([], (_ , [])) Head  *)
-    let result = parser { keyconfig = [] ; machine = [];} Head 
+    (* let result: full_config = parser full_config, parsing_mode, line_count *)
+    let result = parser { keyconfig = [] ; machine = [];} Head 1
     in
 
 
     (* debug print *)
-    print_string "line count: ";
-    (* print_int (List.length (result)); *)
-    print_endline "";
-    (* let print_list (elem) = print_endline elem in
-    List.iter print_list result; *)
+    print_endline "debug print: ";
 
+    print_endline "keyconf: ";
+    let print_keyconf (elem: key) = 
+      begin
+        print_string "key.input: ";
+        print_string elem.input_string;
+        print_string " key.output: ";
+        print_endline elem.output_string;
+      end;
+    in
+    List.iter print_keyconf result.keyconfig;
+
+    print_endline "movelist: ";
+    let print_transition (elem: transition)= 
+      begin
+        print_string "     read: ";
+        print_string elem.read;
+        print_string " to_state: ";
+        print_int elem.to_state;
+        print_string " write: ";
+        print_endline elem.write;
+      end;
+    in let print_state (elem: state) =
+      begin
+        print_string "id: ";
+        print_int elem.id;
+        print_endline "";
+        List.iter print_transition elem.transitions;
+      end;
+    in
+    List.iter print_state result.machine;
 
   with e ->
     raise e
