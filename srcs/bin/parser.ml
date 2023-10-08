1a2af3d038348dda33e2472648123fa551809634
Simon CROS <simon.dc.cros@gmail.com>
2023-10-08 22:38:28 +0200

    Use mode similar code

--- srcs/bin/parser.ml
+++ srcs/bin/parser.ml
@@ -44,101 +44,101 @@ let rec parse (ic : in_channel) (accum : Types.full_config) mode count =
         print_endline "";
         exit 3
       | actions ->
-        let rec add_move actions (state : Types.state) machine =
-          match actions with
-          | head :: tail when key_exists head -> (
-            match
-              List.find_opt
-                (fun (el : Types.transition) -> String.equal head el.read)
-                state.transitions
-            with
-            | None ->
-              (* state has no transition for action_head *)
-              let new_state : Types.state =
-                let new_id =
-                  List.fold_left
-                    (fun a (b : Types.state) -> max a b.id)
-                    0
-                    machine
-                  + 1
-                in
-                { id = new_id; transitions = [] }
-              in
-              let machine =
-                let state : Types.state =
-                  let new_transition : Types.transition =
-                    let msg = if List.length tail == 0 then move_name else "" in
-                    { read = head; to_state = new_state.id; write = msg }
-                  in
-                  { id = state.id
-                  ; transitions =
-                      List.rev_append state.transitions [ new_transition ]
-                  }
-                in
-                List.rev_append
-                  (List.rev_map
-                     (fun (a : Types.state) ->
-                       if a.id == state.id then state else a)
-                     machine)
-                  [ new_state ]
-              in
-              add_move tail new_state machine
-            | Some transi when String.equal transi.write "" -> (
-              match action_tail with
-              | [] ->
-                let msg = move_name in
-                let new_transition : Types.transition =
-                  { read = transi.read
-                  ; to_state = transi.to_state
-                  ; write = msg
-                  }
-                in
-                let transitions_without_current_transi : Types.transition list =
-                  List.filter
-                    (fun (el : Types.transition) ->
-                      not (action_head == el.read))
-                    state.transitions
-                in
-                let updated_current_state : Types.state =
-                  { id = state_id
-                  ; transitions =
-                      List.rev_append
-                        transitions_without_current_transi
-                        [ new_transition ]
-                  }
-                in
-                let machine_without_current_state : Types.machine =
-                  List.filter
-                    (fun (el : Types.state) -> not (state_id == el.id))
-                    machine
-                in
-                List.rev_append
-                  machine_without_current_state
-                  [ updated_current_state ]
-              | h :: t -> add_move h t transi.to_state machine)
-            | Some transi -> (
-              match action_tail with
-              | [] ->
-                (* no tail, write not empty *)
-                (* 2 fully identical action sequences *)
-                print_string "Syntax error on line ";
-                print_int count;
-                print_endline "";
-                print_endline "This full action sequence is already present";
-                exit 3
-              | h :: t -> add_move h t transi.to_state machine))
-          | [] -> machine
-          | _ ->
-            print_string "Syntax error on line ";
-            print_int count;
-            print_string " action: ";
-            print_string (List.nth actions 0);
-            print_endline " has no corresponding keybind";
-            exit 3
-        in
         let new_config : Types.full_config =
+          let rec add_move actions (state : Types.state) machine =
+            let get_state id =
+              List.find (fun (s : Types.state) -> s.id == id) machine
+            in
+            match actions with
+            | head :: tail when key_exists head -> (
+              match
+                List.find_opt
+                  (fun (el : Types.transition) -> String.equal head el.read)
+                  state.transitions
+              with
+              | None ->
+                (* state has no transition for action_head *)
+                let new_state : Types.state =
+                  let new_id =
+                    List.fold_left
+                      (fun a (b : Types.state) -> max a b.id)
+                      0
+                      machine
+                    + 1
+                  in
+                  { id = new_id; transitions = [] }
+                in
+                let machine =
+                  let state : Types.state =
+                    let new_transition : Types.transition =
+                      let msg =
+                        if List.length tail == 0 then move_name else ""
+                      in
+                      { read = head; to_state = new_state.id; write = msg }
+                    in
+                    { id = state.id
+                    ; transitions =
+                        List.rev_append state.transitions [ new_transition ]
+                    }
+                  in
+                  List.rev_append
+                    (List.rev_map
+                       (fun (a : Types.state) ->
+                         if a.id == state.id then state else a)
+                       machine)
+                    [ new_state ]
+                in
+                add_move tail new_state machine
+              | Some transition when String.equal transition.write "" -> (
+                match tail with
+                | [] ->
+                  let state : Types.state =
+                    let transition : Types.transition =
+                      { read = transition.read
+                      ; to_state = transition.to_state
+                      ; write = move_name
+                      }
+                    in
+                    { id = state.id
+                    ; transitions =
+                      List.rev_map
+                      (fun (a : Types.transition) ->
+                        if a.read == transition.read then transition else a)
+                      state.transitions
+                    }
+                  in
+                  (* Repeat without add *)
+                  let machine =
+                    List.rev_map
+                      (fun (a : Types.state) ->
+                        if a.id == state.id then state else a)
+                      machine
+                  in
+                  (* Repeat *)
+                  machine
+                | _ -> add_move tail (get_state transition.to_state) machine)
+              | Some transition -> (
+                match tail with
+                | [] ->
+                  (* no tail, write not empty *)
+                  (* 2 fully identical action sequences *)
+                  print_string "Syntax error on line ";
+                  print_int count;
+                  print_endline "";
+                  print_endline "This full action sequence is already present";
+                  exit 3
+                | h :: t -> add_move tail (get_state transition.to_state) machine))
+            | [] -> machine
+            | _ ->
+              print_string "Syntax error on line ";
+              print_int count;
+              print_string " action: ";
+              print_string (List.nth actions 0);
+              print_endline " has no corresponding keybind";
+              exit 3
+          in
           { keyconfig = accum.keyconfig
-          ; machine = add_move head tail 0 accum.machine
+          ; machine = add_move actions (List.nth accum.machine 0) accum.machine
           }
         in
         parse ic new_config Movelist (count + 1)
